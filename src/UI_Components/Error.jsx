5df42214a0ff2ad30577378e679b794859c7c4a5
DarshanGowda25 <darshangowda2508@gmail.com>
2025-08-31 10:23:05 +0530

    updated store

--- src/UI_Components/Error.jsx
+++ src/UI_Components/Error.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 function Error() {
   return (
-    <div className='h-screen w-full place-content-center bg-[#F2FBF3]'>
-          <img src='/Image/error.png' className='h-[500px] w-[700px] mx-auto'></img>
+    <div className='h-screen w-full place-content-center'>
+          <img src='/Image/No Data Found.png' className='h-[500px] w-[700px] mx-auto'></img>
     
         </div>
   )
